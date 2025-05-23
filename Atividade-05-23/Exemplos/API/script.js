c399517f3c79fd5b746c58bd72cb5c4978ca9f77
SAMARA OLIVEIRA DA SILVA <samara081007@gmail.com>
2025-05-23 11:55:16 -0300

    aula-23-05

---
+++ Atividade-05-23/Exemplos/API/script.js
@@ -0,0 +1,25 @@
+function buscarCep(){
+    const cep= document.getElementById("cep").value;
+    const url= `https://viacep.com.br/ws/${cep}/json`;
+
+    fetch(url)
+        .then((response)=>{
+            return response.json();
+        })
+        .then((data)=>{
+            if (data.erro){
+                alert("CEP ñ encontrado");
+                return;
+            }
+            document.getElementById("logradouro").value= data.logradouro;
+            document.getElementById("bairro").value= data.bairro;
+            document.getElementById("cidade").value= data.localidade;
+            document.getElementById("estado").value= data.estado+ `(${data.uf})`;
+
+
+
+        })
+        .catch((error)=>{
+            alert("erro ao buscar o  cep");
+        })
+}        
