911a8a8a5c7b16db4b8cd10f052b285639ebeb10
Samara Oliveira <163425297+10mara@users.noreply.github.com>
2025-05-30 11:38:23 -0300

    aula-30-05

---
+++ Aula-05-30/src/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'
+function Header(){
+    return (
+        <header className="app-header">
+            <h1>Bem vindo a pag com react</h1>
+        </header>
+    )
+}
+export default Header;
