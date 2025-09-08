01e78b2dec4310ae92f45f53f4c59c957ab6ce7f
Samara Oliveira <163425297+10mara@users.noreply.github.com>
2025-09-08 09:36:05 -0300

    aula-08-09

--- Aula-09-06/src/components/Contador.jsx
+++ Aula-09-06/src/components/Contador.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import styles from "./Contador.module.css"
 export default function Contador(){
+
+  // const estado=  useState(0)
+  // // let contador= estado[0]
+  // // const setContador= estado[1]
+
+  const[contador, setContador]=useState(0)
+
+  function increment(){
+    // setContador(estado[0]+1)
+    setContador(contador=> contador+1)
+    
+  }
+  function decrement(){
+    // setContador(estado[0]-1)
+    setContador(contador=> contador-1)
+    
+  }
+  function reset() {
+    setContador(0);
+  }
+
   return(
     <div className={styles.container}>
       <h1>Contador</h1>
-      <div className={styles.contadorDisplay}>0</div>
+      <div className={styles.contadorDisplay}>{contador}</div>
       <div className={styles.botaoContainer}>
-        <button className={styles.botao}>-</button>
-        <button className={styles.botao}>+</button>
+        <button className={styles.botao} onClick={decrement}>-</button>
+        <button className={styles.botao} onClick={increment}>+</button>
       </div>
-      <button className={styles.botao} > Reset</button>
+      <button className={styles.botao} onClick={reset}>Reset</button>
     </div>
   )
 }
